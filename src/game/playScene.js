f4dd3bd0cc6dacdbd9fe3d6ee74d63e64c9dcf50
spectralflux <simon.fullick@gmail.com>
2020-02-21 16:36:42 +0100

    Added rock collisions

--- src/game/playScene.js
+++ src/game/playScene.js
@@ -5,15 +5,31 @@ var isGameOver;
 var score;
 var boat;
 var boatHealth;
-var rocks;
+var boatSpeed;
+var healthText;
 
 var rockTimer;
 
-function addRock(scene, rockArray) {
-    let rock = scene.physics.add.sprite(400, 0, 'bigrock').setSize(16, 48);
-    rock.setVelocityY(18);
-    rockArray.push(rock);
+var rockGroup;
+
+function resetPlayField() {
+    rockGroup.clear();
 };
+
+function addRock(rockGroup, boatSpeed) {
+    let initX = Phaser.Math.Between(16,800-16);
+    rockGroup.create(initX, 0, 'bigrock').setVelocity(0, boatSpeed);
+};
+
+function hitRock() {
+    console.log("Hit!");
+    boatHealth--;
+    healthText.setText('Boats Remaining: ' + boatHealth);
+    resetPlayField(rockGroup);
+    if (boatHealth <= 0) {
+        isGameOver = true;
+    }
+}
 
 export default {
     key: 'play',
@@ -29,9 +45,9 @@ export default {
         isGameOver = false;
         score = 0;
         boatHealth = 3;
+        boatSpeed = 50;
 
         rockTimer = 100;
-        rocks = [];
     },
 
     create: function () {
@@ -40,21 +56,34 @@ export default {
         boat = this.physics.add.sprite(400, 450, 'boat').setSize(16, 48);
         boat.setCollideWorldBounds(true);
 
-        this.cameras.main.setBackgroundColor(0x1D2B53)
+        this.cameras.main.setBackgroundColor(0x29ADFF)
+
+        healthText = this.add.text(16, 16, 'Boats Remaining: ' + boatHealth, { fontSize: '32px', fill: '#FFFFFF' });
+
+        rockGroup = this.physics.add.group({
+            collideWorldBounds: false
+        });
+
+        this.physics.add.overlap(boat, rockGroup, hitRock, null, this);
+
     },
 
     update: function () {
         if (isGameOver) {
+            console.log("Game Over!");
             this.scene.stop().run('gameOver');
             return;
         }
 
         if (rockTimer <= 0) {
-            addRock(this, rocks);
-            rockTimer = 100;
+            addRock(rockGroup, boatSpeed);
+            rockTimer = 30;
         } else {
             rockTimer--;
         }
+
+        // remove rocks that have left the screen
+        rockGroup.getChildren().filter(rock => rock.getTopCenter().y >= 600).forEach(rock => rock.destroy());
 
         if (trainer.isDoneTraining()) {
             trainer.getPrediction().then(function (prediction) {
