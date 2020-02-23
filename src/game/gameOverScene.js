8518aa7f682db2f674eaacfdbebe80909c6c9037
Simon <spectralflux@users.noreply.github.com>
2020-02-23 18:00:46 +0100

    Add game over text

--- src/game/gameOverScene.js
+++ src/game/gameOverScene.js
@@ -1,8 +1,14 @@
+let gameOverText;
+
 export default {
 
     key: 'gameOver',
 
     create: function () {
+        gameOverText = this.add.text(240, 200, "That's it, we've sunk!", {
+            fontSize: '24px',
+            fill: '#FFF1E8'
+        });
 
     },
 
